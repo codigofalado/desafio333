dfaca4b07b1b3ed9adc618ea52a385a8fff6080d
ednaldo-brito <ednaldo.brito@agencian1.com.br>
2019-08-18 10:18:32 -0300

    html desenvolvido

--- index.php
+++ index.php
@@ -9,13 +9,38 @@
     <link href="assets/css/desafio-01.css" rel="stylesheet">
 
   <body>
-    <header></header>
+    <header class="n-header">
+      <div class="n-header__hastag">
+          <span>#GnewsEmPonto</span>
+      </div>
 
+      <h1 class="n-header__logo">
+        <a href="#" title="">
+          <span>Urgente News</span>
+        </a>
+      </h1>
+    </header>
 
+    <main class="n-content">
+      <img src="" alt="">
+    </main>
+
+    <footer class="n-footer">
+        <div class="n-footer__box-top">
+          <h2>Lava jato no rio</h2>
+        </div>
+        <div class="n-footer__box-middle">
+          <h3>Eike batista volta a ser preso pela polícia federal</h3>
+        </div>
+        <div class="n-footer__box-bottom">
+          <a href="#" title="">
+            <img src="" alt="">
+          </a>
+          <p>nível no globonews play - baixe o seu aplicativo . para envio de sugestões críticas</p>
+          <span>08:16</span>
+        </div>
+    </footer>
   </body>
-
-  <!-- Scripts -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
   <!-- Scripts -->
   <script src="assets/js/desafio-01.js"></script>
