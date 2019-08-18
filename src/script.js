099ff2899004bdbc83f25f683d7b988fe3e05464
Tiago Neves <tiagocorreaneves3@gmail.com>
2019-08-18 20:18:12 -0300

    desafio completado com sucesso!

---
+++ src/script.js
@@ -0,0 +1,24 @@
+let $relogio = document.querySelector('.new-hour')
+
+ function getHour() {
+    let data = new Date()
+    let h = formata(data.getHours())
+    let m = formata(data.getMinutes())
+
+     return h + ":" + m
+}
+
+ function formata(t) {
+    return t < 10 ? "0" + t : t
+}
+
+ function mostraRelogio(atual) {
+    $relogio.textContent = atual
+}
+
+ mostraRelogio(getHour())
+setInterval(() => {
+    mostraRelogio(getHour())
+}, 1000)
+
+
