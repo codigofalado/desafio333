ea6b7c6c5c2b2ee27c0921e3dd6de9a97a41a624
Marcelo Moser <moser28064212@gmail.com>
2019-09-23 23:07:31 -0300

    Added colors

--- 2019-Setembro-Tailwind/marcelomoser/tailwind.config.js
+++ 2019-Setembro-Tailwind/marcelomoser/tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#EB007D',
+        secundary: '#2F2E41',
+        light: '#F2F2F2',
+        dark: '#111017',
+      }
+    }
   },
   variants: {},
   plugins: []
