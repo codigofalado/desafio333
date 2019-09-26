af8e44f586514fbaf4edb3d70c8d4b6b473c551c
gabrielVais <gabriel_lol2017@hotmail.com>
2019-09-26 15:25:09 -0300

    desafio terminado

---
+++ 2019-Setembro-Tailwind/GabrielVais/tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  theme: {
+    extend: {},
+    colors: {
+      rosa: "#FC427B",
+      preto: "#000000",
+      peru: "#DCFE44",
+      azulEscuro:"#40407a",
+      verdeClaro:"#33d9b2",
+      cinza:"#dfe6e9",
+      branco:"#ffff",
+      azulClaro:"#eff7ff",
+      azulText:"#f7f1e3",
+      border:"#3a5bfc",
+      'primary': '#3490dc',
+     'secondary': '#ffed4a',
+     'danger': '#e3342f',
+
+
+
+    }
+  },
+  variants: {
+
+     backgroundColor: ['responsive', 'hover', 'focus'],
+
+  },
+  plugins: []
+};
