fb3297fd07fbac542da15463dccd1bbe2c76b7c7
jor27638084 <jorge77souza@gmail.com>
2020-01-29 23:06:10 -0300

    Exclusão de código comentado

--- 2020-Janeiro-Leitura-Organica/assets/jor27638084/js/acao.js
+++ 2020-Janeiro-Leitura-Organica/assets/jor27638084/js/acao.js
@@ -40,37 +40,3 @@ let startTime = 0
 
 
 
-
-
-
-
-
-
-/* var intervalo;
-
-function iniciarCronometro() {
-    var s = 1;
-    var m = 0;
-    var h = 0;
-    intervalo = window.setInterval(function(){
-        if (s == 60){
-            m++; s = 0;
-        }
-        if (m == 60){
-            h++; s = 0;m = 0;
-        }
-        if ( h < 10) {document.getElementById("hora").value = "0" + h + "h";}
-        else {document.getElementById("hora").value = h + "h";}
-        if (s < 10) {document.getElementById("segundo").value = "0" + s + "s";}
-        else {document.getElementById("segundo").value = s + "s";}
-        if (m < 10) {document.getElementById("minuto").value = "0" + m + "m";}
-        else {document.getElementById("minuto").value = m + "m";}
-        s++
-
-    }, 1000)
-    
-}
-
-function pararCronometro(){
-    window.clearInterval(intervalo);
-} */
