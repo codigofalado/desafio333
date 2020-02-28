16c8f2afbe57da57a999a624d817e74775f49b5c
rickecr <richelton14@gmail.com>
2020-02-27 22:03:37 -0300

    iniciando com o timer de leitura

--- 2020-Janeiro-Leitura-Organica/rickecr/src/components/VelLeitura/index.js
+++ 2020-Janeiro-Leitura-Organica/rickecr/src/components/VelLeitura/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import Button from "@material-ui/core/Button";
 import TextCard from '../TextCard';
@@ -6,38 +6,89 @@ import ResultDialog from '../ResultDialog';
 
 import './index.css';
 
-export default function VelLeitura() {
-	const [isTestInit, setIsTestInit] = useState(false);
-	const [open, setOpen] = React.useState(false);
+export default class VelLeitura extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isTestInit: false,
+      open: false,
+      minutes: 0,
+      seconds: 0,
+      time: 0,
+    }
+  }
 
-	const handleClose = () => {
-		setIsTestInit(false);
-		setOpen(false);
-	};
+	handleClose() {
+    this.setState({
+      isTestInit: false,
+      open: false,
+    });
+	}
 
-	return (
-		<div className="div-text">
-			<div className="div-btn">
-					<Button id="btn" href="#div-text" onClick={() => setIsTestInit(true)}>
-						Começar
-					</Button>
-				
-				{/* <Button href="#div-text" onClick={() => setIsTestInit(true)}>
-					<span>Começar</span>
-				</Button> */}
-			</div>
+  startClock() {
+    this.myInterval = setInterval(() => {
+      const { minutes, seconds } = this.state;
 
-			{isTestInit &&
-			<div id="div-text">
-				<TextCard />
-				<div className="div-btn">
-					<button id="btn" onClick={() => {setOpen(true)}}>Terminei</button>
-				</div>
-			</div>
+      if (seconds >= 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds + 1
+      }))
 			}
 
-			{open && <ResultDialog onClosed={handleClose} open={open} />}
+			if (seconds >= 59) {
+        this.setState(({ minutes }) => ({
+          minutes: minutes + 1,
+          seconds: 0
+      }))
+			}
+		}, 1000)
+  }
 
-		</div>
-	);
+	readStart() {
+    this.setState({
+      isTestInit: true
+    });
+
+    this.startClock();
+	}
+
+	readComplete() {
+    this.setState({
+      open: true
+    });
+
+    clearInterval(this.myInterval);
+	}
+
+  render() {
+    const { minutes, seconds, isTestInit, open } = this.state;
+
+    return (
+      <div className="div-text">
+        <div className="div-btn">
+            <Button id="btn" href="#div-text" onClick={() => this.readStart()}>
+              Começar
+            </Button>
+          
+          {/* <Button href="#div-text" onClick={() => setIsTestInit(true)}>
+            <span>Começar</span>
+          </Button> */}
+        </div>
+  
+        {isTestInit &&
+        <div id="div-text">
+          <TextCard />
+          <div className="div-btn">
+            <button id="btn" onClick={() => this.readComplete()}>Terminei</button>
+          </div>
+        </div>
+        }
+  
+        {open && <ResultDialog onClosed={this.handleClose} open={open} />}
+  
+        <span>{ minutes }:{ seconds < 10 ? `0${ Number(seconds) }` : Number(seconds) }</span>
+  
+      </div>
+    );
+  }
 }
