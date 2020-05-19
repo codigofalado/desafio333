ac264a274823e30dcd8f53c5495eb281357aa8aa
Henrique Miranda <enrque.ns@gmail.com>
2020-05-19 18:45:29 -0300

    :bulb: chore: add colors

--- 2020-QuarentenaTech/os-3-pontinhos/src/constants.js
+++ 2020-QuarentenaTech/os-3-pontinhos/src/constants.js
@@ -6,29 +6,29 @@ const TIME_INTERVAL = 1000;
 
 const I_MODEL = {
   shape: [[1, 1, 1, 1]],
-  color: "#FFFF",
+  color: "#00ADEE",
   width: 4,
   height: 1,
 };
 
 const J_MODEL = {
   shape: [
-    [0, 0, 1],
     [1, 1, 1],
+    [1, 0, 0],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#F6921E",
   width: 3,
   height: 2,
 };
 
 const L_MODEL = {
   shape: [
-    [1, 0, 0],
     [1, 1, 1],
+    [0, 0, 1],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#1B75BB",
   width: 3,
   height: 2,
 };
@@ -38,7 +38,7 @@ const O_MODEL = {
     [1, 1],
     [1, 1],
   ],
-  color: "#FFFF",
+  color: "#FFF100",
   width: 2,
   height: 2,
 };
@@ -49,7 +49,7 @@ const S_MODEL = {
     [1, 1, 0],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#8BC53F",
   width: 3,
   height: 2,
 };
@@ -60,7 +60,7 @@ const Z_MODEL = {
     [0, 1, 1],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#EC1C24",
   width: 3,
   height: 2,
 };
@@ -71,7 +71,7 @@ const T_MODEL = {
     [0, 1, 0],
     //[0, 0, 0],
   ],
-  color: "#FFFF",
+  color: "#652D90",
   width: 3,
   height: 2,
 };
