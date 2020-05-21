fcaeb49ce556e5cc15f95f648c57eb102962e4c6
João Lima <jpalvesl101@gmail.com>
2020-05-21 09:25:56 +0000

    estilizando bottomTab

--- 2020-QuarentenaTech/jpalvesl/src/Navigation.js
+++ 2020-QuarentenaTech/jpalvesl/src/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,10 +12,33 @@ import Credits from './screens/Credits';
 function Navigation() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Game" component={Game} />
-        <Tab.Screen name="Ranking" component={Ranking} />
-        <Tab.Screen name="Credits" component={Credits} />
+      <Tab.Navigator
+        tabBarOptions={{
+          activeTintColor: '#f0f',
+          inactiveTintColor: '#0ff',
+          style: {
+            backgroundColor: '#000'
+          }
+        }}
+      >
+        <Tab.Screen name="Game" component={Game} 
+          options={{
+            title: 'Jogo',
+            tabBarIcon: ({ color, size }) => (<Ionicons name="logo-game-controller-a" color={color} size={size} />),
+          }}
+        />
+        <Tab.Screen name="Ranking" component={Ranking} 
+          options={{
+            title: 'Ranking',
+            tabBarIcon: ({ color, size }) => (<Ionicons name="ios-trophy" color={color} size={size} />),
+          }}
+        />
+        <Tab.Screen name="Credits" component={Credits} 
+          options={{
+            title: 'Créditos',
+            tabBarIcon: ({ color, size }) => (<Ionicons name="ios-person" color={color} size={size} />),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   )
