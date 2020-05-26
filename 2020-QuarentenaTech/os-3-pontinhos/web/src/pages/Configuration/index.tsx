4a2e948caaefe49a7660eb6ae5c513fc1f3f11ad
Henrique Miranda <enrque.ns@gmail.com>
2020-05-25 21:02:03 -0300

    :sparkles: feat: create configuration page

--- 2020-QuarentenaTech/os-3-pontinhos/web/src/pages/Configuration/index.tsx
+++ 2020-QuarentenaTech/os-3-pontinhos/web/src/pages/Configuration/index.tsx
@@ -1,14 +1,78 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { useHistory } from 'react-router-dom';
 
 import Header from '../../components/Header';
+import { useConfig } from '../../hooks/config';
 
-import { Container } from './styles';
+import * as S from './styles';
 
 const Configuration: React.FC = () => {
+  const {
+    difficulty,
+    gridEnabled,
+    phantomPieceEnabled,
+    toggleGrid,
+    togglePhantomPiece,
+    increseDifficulty,
+    decreseDifficulty,
+    saveConfig,
+  } = useConfig();
+
+  const history = useHistory();
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    saveConfig();
+
+    history.push('/');
+  }
+
   return (
     <>
       <Header />
-      <Container />
+      <S.Container>
+        <h1>Configuração</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="difficulty">
+            Dificuldade inicial
+            <div>
+              <button type="button" onClick={decreseDifficulty}>
+                <FaChevronLeft />
+              </button>
+              <input readOnly type="text" id="difficulty" value={difficulty} />
+              <button type="button" onClick={increseDifficulty}>
+                <FaChevronRight />
+              </button>
+            </div>
+          </label>
+
+          <label htmlFor="phantomPiece">
+            Peça fantasma
+            <input
+              type="checkbox"
+              id="phantomPiece"
+              checked={phantomPieceEnabled}
+              onChange={togglePhantomPiece}
+            />
+            <span />
+          </label>
+
+          <label htmlFor="grid">
+            Grid
+            <input
+              type="checkbox"
+              id="grid"
+              checked={gridEnabled}
+              onChange={toggleGrid}
+            />
+            <span />
+          </label>
+
+          <S.SubmitButton>Salvar</S.SubmitButton>
+        </form>
+      </S.Container>
     </>
   );
 };
--- 2020-QuarentenaTech/os-3-pontinhos/web/src/pages/Configuration/styles.ts
+++ 2020-QuarentenaTech/os-3-pontinhos/web/src/pages/Configuration/styles.ts
@@ -1,3 +1,127 @@
 import styled from 'styled-components';
 
-export const Container = styled.main``;
+import { rem, opacify } from 'polished';
+
+export const Container = styled.main`
+  max-width: ${({ theme }) => theme.sizes.maxWidth};
+  margin: 0 auto;
+  padding: 32px;
+
+  h1 {
+    text-align: center;
+    font-size: ${rem('48px')};
+    letter-spacing: 0.2em;
+
+    margin: 48px 0;
+  }
+
+  form {
+    font-size: ${rem('36px')};
+    letter-spacing: 0.05em;
+
+    max-width: 715px;
+    margin: 0 auto;
+
+    label {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+
+      &:not(:first-of-type) {
+        cursor: pointer;
+      }
+
+      & + label {
+        margin-top: 64px;
+      }
+    }
+
+    input[type='text'] {
+      max-width: 180px;
+      height: 32px;
+      background: transparent;
+      border: 0;
+
+      text-align: center;
+      font-size: ${rem('36px')};
+      color: ${({ theme }) => theme.colors.secondaryText};
+
+      pointer-events: none;
+      outline: none;
+    }
+
+    button[type='button'] {
+      background: transparent;
+      cursor: pointer;
+      border: 0;
+
+      svg {
+        height: 24px;
+        width: 24px;
+      }
+    }
+
+    span {
+      height: 32px;
+      width: 32px;
+
+      background: ${({ theme }) => theme.colors.secondaryText};
+      position: relative;
+
+      &::before {
+        content: '';
+        position: absolute;
+
+        width: 8px;
+        height: 16px;
+        margin: 0 4px;
+
+        top: 3px;
+        left: 6px;
+
+        border-right: 4px solid ${({ theme }) => theme.colors.active};
+        border-bottom: 4px solid ${({ theme }) => theme.colors.active};
+        opacity: 0;
+
+        transition: all 0.3s ease-out;
+      }
+    }
+
+    input[type='checkbox'] {
+      display: none;
+      cursor: pointer;
+
+      &:checked ~ span::before {
+        transform: rotate(45deg);
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  font-size: ${rem('24px')};
+  letter-spacing: 0.25rem;
+  text-transform: uppercase;
+
+  border: 0;
+  margin-top: 64px;
+  height: 40px;
+  width: 100%;
+
+  background: ${({ theme }) => opacify(-0.2, theme.colors.active)};
+  color: ${({ theme }) => theme.colors.backgroundDark};
+  transition: all 0.2s ease-out;
+
+  &:hover {
+    transform: scale(1.05);
+    background: ${({ theme }) => theme.colors.active};
+  }
+
+  &:active {
+    transform: scale(0.95);
+    background: ${({ theme }) => theme.colors.active};
+  }
+`;
