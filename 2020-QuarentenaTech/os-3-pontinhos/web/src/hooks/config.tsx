264106d4cebbb650fb61f35caca914776789278d
Henrique Miranda <enrque.ns@gmail.com>
2020-05-25 21:01:44 -0300

    :bulb: chore: create hook for game config

---
+++ 2020-QuarentenaTech/os-3-pontinhos/web/src/hooks/config.tsx
@@ -0,0 +1,100 @@
+import React, { createContext, useContext, useState, useMemo } from 'react';
+
+interface Config {
+  difficulty: string;
+  gridEnabled: boolean;
+  phantomPieceEnabled: boolean;
+  togglePhantomPiece(): void;
+  toggleGrid(): void;
+  increseDifficulty(): void;
+  decreseDifficulty(): void;
+  saveConfig(): void;
+}
+
+const DIFFICULTY = ['Facíl', 'Medio', 'Díficil'];
+
+const ConfigContext = createContext<Config>({} as Config);
+
+const ConfigProvider: React.FC = ({ children }) => {
+  const [difficulty, setDifficulty] = useState(() => {
+    const response = localStorage.getItem('@tetris333:config:difficulty');
+    return response ? JSON.parse(response) : 1;
+  });
+
+  const [phantomPieceEnabled, setPhantomPieceEnabled] = useState(() => {
+    const response = localStorage.getItem('@tetris333:config:phantomPiece');
+    return response ? JSON.parse(response) : true;
+  });
+
+  const [gridEnabled, setGridEnabled] = useState(() => {
+    const response = localStorage.getItem('@tetris333:config:grid');
+    return response ? JSON.parse(response) : true;
+  });
+
+  const formattedDifficulty = useMemo(() => DIFFICULTY[difficulty], [
+    difficulty,
+  ]);
+
+  function togglePhantomPiece(): void {
+    setPhantomPieceEnabled(!phantomPieceEnabled);
+  }
+
+  function toggleGrid(): void {
+    setGridEnabled(!gridEnabled);
+  }
+
+  function increseDifficulty(): void {
+    if (difficulty < 2) {
+      setDifficulty(difficulty + 1);
+    }
+  }
+
+  function decreseDifficulty(): void {
+    if (difficulty > 0) {
+      setDifficulty(difficulty - 1);
+    }
+  }
+
+  function saveConfig(): void {
+    localStorage.setItem('@tetris333:config:grid', JSON.stringify(gridEnabled));
+
+    localStorage.setItem(
+      '@tetris333:config:difficulty',
+      JSON.stringify(difficulty),
+    );
+
+    localStorage.setItem(
+      '@tetris333:config:phantomPiece',
+      JSON.stringify(phantomPieceEnabled),
+    );
+  }
+
+  return (
+    <ConfigContext.Provider
+      value={{
+        difficulty: formattedDifficulty,
+        gridEnabled,
+        phantomPieceEnabled,
+        toggleGrid,
+        togglePhantomPiece,
+        increseDifficulty,
+        decreseDifficulty,
+        saveConfig,
+      }}
+    >
+      {children}
+    </ConfigContext.Provider>
+  );
+};
+
+function useConfig(): Config {
+  const context = useContext(ConfigContext);
+
+  if (!context) {
+    throw new Error('useConfig must be used within an ConfigProvider');
+  }
+
+  return context;
+}
+
+export { useConfig, ConfigProvider };
--- 2020-QuarentenaTech/os-3-pontinhos/web/src/hooks/index.tsx
+++ 2020-QuarentenaTech/os-3-pontinhos/web/src/hooks/index.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import theme from '../styles/themes';
+import { ConfigProvider } from './config';
 
 const AppProvider: React.FC = ({ children }) => (
-  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <ConfigProvider>{children}</ConfigProvider>
+  </ThemeProvider>
 );
 
 export default AppProvider;
