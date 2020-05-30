99d8287126458ed0e10ea776f509549a8170bb53
gariandi <32017473+gariandi@users.noreply.github.com>
2020-05-30 20:40:18 -0300

    Update ProximaJogada_.m

--- 2020-QuarentenaTech/wolverines/src/scripts/ProximaJogada_.m
+++ 2020-QuarentenaTech/wolverines/src/scripts/ProximaJogada_.m
@@ -28,6 +28,7 @@ Tabuleiro = csvread('Tabuleiro.csv');
 if a < dificuldade
   
   Tabuleiro = NaoGanharas(Tabuleiro);   % IA - ganha em 90% dos casos, empata em 8%, só deixa user vencer 2%
+  load cod; disp('cod = '); disp(cod); %output da estratégia usada no servidor
 
 else
 
@@ -36,6 +37,7 @@ else
 end
 
 csvwrite('Tabuleiro.csv',Tabuleiro); %Tabuleiro vai ser 3x3 durante o jogo e 1x1 no final
+
 
 q = 1;
 
