6e985adc007b1921f6cfc5b388895cbbc8068053
wdwf <w_dwf@hotmail.com>
2020-05-31 15:54:32 -0300

    logica de status

--- 2020-QuarentenaTech/wolverines/src/public/scripts/script.js
+++ 2020-QuarentenaTech/wolverines/src/public/scripts/script.js
@@ -88,7 +88,15 @@ http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 http.onreadystatechange = function() {//Call a function when the state changes.
   if(http.readyState == 4 && http.status == 200) {
 
-    console.log(http.responseText);
+   
+    if(http.responseText == "") {
+      console.log("Perdeu Otario");
+    } else if(http.responseText == "-1") {
+      console.log("Empate");
+    } else {
+      console.log("Vitoria carai");
+    }
+
     
 
     state.matrix = http.responseText
