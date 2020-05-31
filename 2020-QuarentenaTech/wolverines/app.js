dcd1ba418506de9760a7f824c73e9d8300f26992
viniciusviana <vinicius.s.viana>
2020-05-31 13:51:36 -0300

    Experimentações assíncronas

--- 2020-QuarentenaTech/wolverines/app.js
+++ 2020-QuarentenaTech/wolverines/app.js
@@ -21,67 +21,27 @@ app.get('/', indexRouter);
 app.post('/proximaJogada', async function(req, res) {
 
   await fs.writeFile('src/scripts/Tabuleiro.csv', req.body.csvContent, (err) => {
+    
     if (err) throw err;    
     console.log('Gravando tabuleiro...');
     console.log(req.body.csvContent);
+
   });
 
   await exec('octave-cli --eval "cd src/scripts; ProximaJogada_(' + req.body.difficult + ')"', async (err, stdout, stderr) => {
-    if (err) throw err;
     
+    if (err) throw err;    
     console.log('Executando octave...');
     
     await fs.readFile('src/scripts/Tabuleiro.csv', "utf8", (err, data) => {      
       if (err) throw err;
       console.log('Retornando tabuleiro...');
+      res.send(data)
     });
 
   });  
 
 });
-
-// app.post('/obterParametroParaScriptOctave', function(req, res) {
-  
-//   fs.readFile('src/scripts/Tabuleiro.csv', "utf8", (err, data) => {      
-//     if (err) throw err;
-//     console.log("Gravando...")
-//     console.log(data)
-
-//     fs.writeFile('src/scripts/Tabuleiro.csv', req.body.csvContent, (err) => {
-//       if (err) throw err;    
-//       console.log("Conteúdo a ser gravado:");
-//       console.log(req.body.csvContent)
-
-//       fs.readFile('src/scripts/Tabuleiro.csv', "utf8", (err, data) => {      
-//         if (err) throw err;
-//         console.log("Gravado...")
-//         console.log(data)
-        
-//         exec('octave-cli --eval "cd src/scripts; ProximaJogada_(' + req.body.difficult + ')"', (error, stdout, stderr) => {
-//           if (error) {
-//             // console.error(`exec error: ${error}`);
-//             return;
-//           }
-      
-//           // console.log(`stdout: ${stdout}`);
-//           // console.error(`stderr: ${stderr}`);
-      
-//           fs.readFile('src/scripts/Tabuleiro.csv', "utf8", (err, data) => {      
-//             if (err) throw err;
-//             console.log("Octave processou.")
-//             console.log(data);
-//             res.send(data)
-//           });
-          
-//         }); 
-
-//       });
-
-//     });
-
-//   });   
-
-// })
 
 app.listen(3000, err => {
   console.log("Server is listening on 3000");
