e56925828523bafdad3670fe82a16794871e5df5
wdwf <w_dwf@hotmail.com>
2020-05-31 15:45:13 -0300

    inicio da logica de status

--- 2020-QuarentenaTech/wolverines/src/public/scripts/script.js
+++ 2020-QuarentenaTech/wolverines/src/public/scripts/script.js
@@ -91,6 +91,9 @@ http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 http.onreadystatechange = function() {//Call a function when the state changes.
   if(http.readyState == 4 && http.status == 200) {
 
+    console.log(http.responseText);
+    
+
     state.matrix = http.responseText
       .replace("\n", ",")
       .replace("\n", ",")
