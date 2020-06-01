c9ac13db4ed807e4c87d73894534eaf814f22a97
wdwf <w_dwf@hotmail.com>
2020-05-31 21:30:54 -0300

    adição do model

--- 2020-QuarentenaTech/wolverines/src/public/scripts/script.js
+++ 2020-QuarentenaTech/wolverines/src/public/scripts/script.js
@@ -153,7 +153,10 @@ function choiceDificultOption(elem) {
 let valor = elem.className;
 if( valor == "facil" ) state.difficult = 0.3;
 else if( valor == "medio" ) state.difficult = 0.6;
-else state.difficult = 0.9;
+else {
+  aviso();
+  state.difficult = 0.9;
+} 
 
 // if( dificuldadeEscolhida == undefined ) {
 //   dificuldadeEscolhida = 0.3;
@@ -443,10 +446,30 @@ if (valor == 0){
 let puxaPlacar = document.querySelector('.puxaPlacar');
 puxaPlacar.addEventListener('click', choiceEventPlacar);
 
-function reset() {
-  console.log("Eu resetei tudo só que vc não viu");
+
+//--------------------------------------
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+let aviso = function() {
+  modal.style.display = "block";
 }
 
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
