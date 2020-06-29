95fd6f221bf0d245cb30e334d62797bccc163bab
FranciscoCandido <franciscocandidoal@gmail.com>
2020-06-29 02:55:38 -0300

    :sparkles: Comando "Avatar" criado

---
+++ 2020-Bot-Discord/franciscocandido/src/commands/utils/Avatar.ts
@@ -0,0 +1,51 @@
+import { Command } from 'discord-akairo';
+import { Message, MessageEmbed, User } from 'discord.js';
+
+export default class AvatarCommand extends Command {
+	constructor() {
+		super('avatar', {
+			aliases: ['avatar', 'profilePic'],
+			category: 'utils',
+			ratelimit: 3,
+			channel: 'guild',
+			args: [
+				{
+					id: 'user',
+					type: 'user',
+					default: (message: { author: any }) => message.author
+				}
+			]
+		});
+	}
+
+	public exec(message: Message, { user }: { user: User }) {
+		try {
+			let avatarEmbed = new MessageEmbed()
+				.setColor('00CEB3')
+				.setTitle(`:frame_photo: ${user.username}`)
+				.setDescription(
+					`Clique **[aqui](${user.avatarURL({
+						format: 'jpg',
+						dynamic: true,
+						size: 4096
+					})})** para baixar a imagem.`
+				)
+				.setImage(
+					user.displayAvatarURL({
+						format: 'webp',
+						dynamic: true,
+						size: 2048
+					})
+				)
+				.setFooter(`Avatar requisitado por ${message.author.username}`)
+				.setTimestamp();
+
+			return message.channel.send(avatarEmbed);
+		} catch (erro) {
+			console.log('[ERRO]', erro);
+			return message.channel.send(
+				':confused: Algo de errado aconteceu, tente novamente mais tarde.'
+			);
+		}
+	}
+}
