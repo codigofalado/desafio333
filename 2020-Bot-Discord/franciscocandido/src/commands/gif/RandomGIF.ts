6723dbbbf4d398576362f2b83e78204ac44e2af9
FranciscoCandido <franciscocandidoal@gmail.com>
2020-06-29 08:20:05 -0300

    :sparkles: Comando "RandomGIF" criado
    Mostra um GIF aleatório do giphy.com

---
+++ 2020-Bot-Discord/franciscocandido/src/commands/gif/RandomGIF.ts
@@ -0,0 +1,39 @@
+import { Command } from 'discord-akairo';
+import { Message, MessageEmbed } from 'discord.js';
+import axios from 'axios';
+
+const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
+
+export default class RandomGifCommand extends Command {
+	public constructor() {
+		super('randomGif', {
+			aliases: ['randomGif', 'gif'],
+			category: 'gif',
+			ratelimit: 3,
+			channel: 'guild'
+		});
+	}
+
+	public async exec(message: Message) {
+		try {
+			let gif = await axios.get(
+				`https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}&rating=g`
+			);
+
+			let randomGif = gif.data.data.images.original.url;
+
+			return message.channel.send(
+				new MessageEmbed()
+					.setColor('00CEB3')
+					.setImage(randomGif)
+					.setFooter(`GIF Requisitado por ${message.author.username}`)
+					.setTimestamp()
+			);
+		} catch (erro) {
+			console.log('[ERRO]', erro);
+			return message.channel.send(
+				':confused: Algo de errado aconteceu, tente novamente mais tarde.'
+			);
+		}
+	}
+}
