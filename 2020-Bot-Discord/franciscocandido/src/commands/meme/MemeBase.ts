4f0f186437e1375b78451f10eb8b205fdc65d4b6
FranciscoCandido <franciscocandidoal@gmail.com>
2020-07-06 08:13:13 -0300

    :goal_net: Capturando erros

--- 2020-Bot-Discord/franciscocandido/src/commands/meme/MemeBase.ts
+++ 2020-Bot-Discord/franciscocandido/src/commands/meme/MemeBase.ts
@@ -21,37 +21,44 @@ export default class MemeBase extends Command {
 	}
 
 	public async exec(message: Message) {
-		let meme = await axios
-			.get('https://api.imgflip.com/get_memes')
-			.then((data) => data.data);
+		try {
+			let meme = await axios
+				.get('https://api.imgflip.com/get_memes')
+				.then((data) => data.data);
 
-		let memeArray: [] = meme.data.memes;
-		let randomNumber: number = Math.floor(Math.random() * memeArray.length);
-		let randomMeme: randomMeme = meme.data.memes[randomNumber];
-		console.log(randomMeme);
+			let memeArray: [] = meme.data.memes;
+			let randomNumber: number = Math.floor(Math.random() * memeArray.length);
+			let randomMeme: randomMeme = meme.data.memes[randomNumber];
+			console.log(randomMeme);
 
-		return message.channel.send(
-			new MessageEmbed()
-				.setColor('00CEB3')
-				.setTitle(`Nome: ${randomMeme.name}`)
-				.addFields(
-					{
-						name: ':id: ID no Imgflip',
-						value: `${randomMeme.id}`,
-						inline: true
-					},
-					{
-						name: ':frame_photo: Tamanho',
-						value: `${randomMeme.width} x ${randomMeme.height}`,
-						inline: true
-					}
-				)
-				.setDescription(
-					`Clique [aqui](${randomMeme.url}) para baixar a imagem.`
-				)
-				.setImage(`${randomMeme.url}`)
-				.setFooter(`Imagem requisitada por ${message.author.username}`)
-				.setTimestamp()
-		);
+			return message.channel.send(
+				new MessageEmbed()
+					.setColor('00CEB3')
+					.setTitle(`Nome: ${randomMeme.name}`)
+					.addFields(
+						{
+							name: ':id: ID no Imgflip',
+							value: `${randomMeme.id}`,
+							inline: true
+						},
+						{
+							name: ':frame_photo: Tamanho',
+							value: `${randomMeme.width} x ${randomMeme.height}`,
+							inline: true
+						}
+					)
+					.setDescription(
+						`Clique [aqui](${randomMeme.url}) para baixar a imagem.`
+					)
+					.setImage(`${randomMeme.url}`)
+					.setFooter(`Imagem requisitada por ${message.author.username}`)
+					.setTimestamp()
+			);
+		} catch (erro) {
+			console.log('[ERRO]', erro);
+			return message.channel.send(
+				':confused: Algo de errado aconteceu, tente novamente mais tarde.'
+			);
+		}
 	}
 }
