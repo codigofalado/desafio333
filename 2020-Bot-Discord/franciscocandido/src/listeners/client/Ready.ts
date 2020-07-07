5730527b7590c13673c8ac228b11091d15fee90b
FranciscoCandido <franciscocandidoal@gmail.com>
2020-07-07 15:42:10 -0300

    :sparkles: Listener para "Ready"

---
+++ 2020-Bot-Discord/franciscocandido/src/listeners/client/Ready.ts
@@ -0,0 +1,31 @@
+import { Listener } from 'discord-akairo';
+
+export default class Ready extends Listener {
+	public constructor() {
+		super('ready', {
+			emitter: 'client',
+			event: 'ready',
+			category: 'client'
+		});
+	}
+
+	public exec() {
+		let activities: any = [
+			{
+				text: '<b!ajuda> para ver meus comandos',
+				type: 'WATCHING'
+			},
+			{
+				text: '🎧 BMO (Adventure Time) - Friends Song',
+				type: 'LISTENING'
+			}
+		];
+
+		setInterval(() => {
+			let randomStatus = Math.floor(Math.random() * activities.length);
+
+			let activitie = activities[randomStatus];
+			this.client.user.setActivity(activitie.text, { type: activitie.type });
+		}, 2e4);
+	}
+}
