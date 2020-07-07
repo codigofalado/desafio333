a72f5f39f5b2327b4686fa03ab713155af734d2c
FranciscoCandido <franciscocandidoal@gmail.com>
2020-07-07 10:40:50 -0300

    :sparkles: Comando "Stonks" refeito utilizando jimp

---
+++ 2020-Bot-Discord/franciscocandido/src/commands/meme/Stonks.ts
@@ -0,0 +1,55 @@
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+import fs from 'fs';
+import jimp from 'jimp';
+
+export default class Stonks extends Command {
+	public constructor() {
+		super('stonks', {
+			aliases: ['stonks', 'stonksMeme'],
+			category: 'meme',
+			ratelimit: 1,
+			channel: 'guild',
+			args: [
+				{
+					id: 'text',
+					type: 'string',
+					match: 'content',
+					prompt: {
+						start: 'Qual a frase?'
+					}
+				}
+			]
+		});
+	}
+
+	public async exec(message: Message, { text }) {
+		try {
+			let templateUrl =
+				'https://cdn.discordapp.com/attachments/730043354661781536/730043505958715402/stonks.jpg';
+
+			let fileName = `${message.author.username}-${message.author.id}.jpg`;
+
+			let awaitMessage = await message.channel.send(
+				':hourglass_flowing_sand: Seu meme ta quase pronto...'
+			);
+
+			await jimp.read(templateUrl).then(async (template) => {
+				let font = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
+
+				template.print(font, 0, 0, text);
+				template.write(fileName);
+			});
+
+			await message.channel.send('', { files: [`${fileName}`] });
+
+			awaitMessage.delete();
+			fs.unlinkSync(fileName);
+		} catch (erro) {
+			console.log('[ERRO]', erro);
+			return message.channel.send(
+				':confused: Algo de errado aconteceu, tente novamente mais tarde.'
+			);
+		}
+	}
+}
