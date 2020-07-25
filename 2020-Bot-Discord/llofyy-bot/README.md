0c044b8425d52e503a3eef19649ccadc00353910
Lucas André <30734697+llofyy@users.noreply.github.com>
2020-07-25 17:44:49 -0300

    Finalizando o bot

---
+++ 2020-Bot-Discord/llofyy-bot/README.md
@@ -0,0 +1,55 @@
+# llofyy-bot desafio333
+
+# Clonando e modificando o bot
+
+- Faça o clone do repositório em sua máquina `git clone https://github.com/llofyy/llofyy-bot.git`
+
+- Acesse a pasta do projeto em seu editor ou IDE.
+
+- Adciione as keys que precisa para o funcionamento do bot (Key Tenor (API GIF)) (Discord (TOKEN PARA O BOT)) (RIOT Developer(API KEY PARA OS DADOS DO LEAGUE OF LEGENDS)).
+
+- Após modificar todas as chaves de acesso, envie o bot para seu servidor ou execute em sua máquina local.
+
+- Abra o terminal ou use o console do servidor e execute o comando `npm start` e veja o bot funcionando.
+
+
+# COMANDOS:
+
+### !gif ou !gif [Alguma palavra chave]
+
+O comando !gif irá retornar um gif aleatório no canal onde o bot estiver funcionando. Há duas maneiras de se utilizar esse comando, a primeira maneira é p comando `!gif` que irá retornar um gif totalmente aleatório. Também existe o comando `!gif discord` (A palavra discord é apenas um exemplo, podendo utilizar qualquer palavra que quiser). Esse comando retorna gif aleatório utilizando como parâmetro a palavra que o usuário digitou para um tema.
+
+### !rastreio [código de rastreio]
+
+Esse comando irá retornar informações para o usuário de como está a situação do pacote do código de rastreio utilizado.
+
+### !dollar e !bitcoin
+
+Usando esses comandos, é possível ficar por dentro da cotação do dolár (através do comando `!dollar`) e da cotação do Bitcoin (através do comando `!bitcoin`).
+
+### !covid
+
+Comando usado para receber as últimas atualizações dos dados sobre o Covid-19. Usando o comando `!covid -g` é mostrado os dados globais da doença e usando o comando `!covid -br` é mostrado os dados sobre a doença no Brasil.
+
+# !avatar
+
+É um comando simples apenas para o usuário mostrar a todos no chat seu avatar.
+
+# F no chat clã
+
+Utilizando apenas a letra `f` ou `F` o bot retorna uma imagem de R.I.P dando referência a brincadeira "F no chat clã".
+
+# !elo
+
+Uma das funcionalidades mais interessantes que ajuda os jogadores de League Of Legends. Caso você queira mostrar ao canal onde está ou simplesmente quer ver os dados de um jogador específico aqui no Brasil, basta utilizar o comando `!elo [nome de invocador]` e o bot retornará os dados do jogador indicado. Trazendo algumas informações interessantes como:
+
+## Elo do jogador na SoloQ Duo e na Flex (Caso o invocador tenha).
+
+## Nível da conta
+
+## Número de vitórias e derrotas.
+
+## Quantidade de Pdl's da conta.
+
+Esse é um bot desenvolvido como forma de estudo e também para a participação do [desafio333](https://github.com/codigofalado/desafio333). Meu objetivo principal não é ganhar o desafio e sim agregar mais conhecimento na minha caminhada de aprendizado. Com o término desse projeto eu pude ver um avanço muito bacana de meus conhecimentos e agradeço muito essa oportunidade. Boa sorte a todos nós, não apenas para quem vencer o desafio, mas em toda a nossa caminhada profissional. 
+
--- 2020-Bot-Discord/llofyy-bot/bot.js
+++ 2020-Bot-Discord/llofyy-bot/bot.js
@@ -1,10 +1,15 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const {rastro} = require('rastrojs');
+const { Client, MessageAttachment, MessageEmbed } = require('discord.js');
+const client = new Client();
+const { rastro } = require('rastrojs');
 const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const fs = require('fs');
+const TeemoJS = require('teemojs');
+const lolKey = "SUA KEY DE RIOT DEVELOPER AQUI";
+const api = TeemoJS(lolKey);
+
 
 const Tenor = require('tenorjs').client({
-    "Key": "",
+    "Key": "SUA KEY DA API DO TERNOR AQUI",
     "Filter": "off",
     "Locale": "pt_br",
     "MediaFilter": "minimal",
@@ -12,7 +17,7 @@ const Tenor = require('tenorjs').client({
 });
 
 
-client.login("");
+client.login("SEU TOKEN DO DISCORD AQUI");
 
 client.on('ready', () => {
     console.log(`Logado como ${client.user.username}!`);
@@ -22,7 +27,7 @@ client.on('ready', () => {
 
 // MOSTRA GIFS ALEATÓRIOS COM OU SEM PALAVRA CHAVE
 client.on('message', msg => {
-    if(msg.content == "!gif"){
+    if (msg.content == "!gif") {
         Tenor.Search.Random(msg.content, "1").then(Results => {
             Results.forEach(Post => {
                 msg.reply(Post.url);
@@ -30,11 +35,12 @@ client.on('message', msg => {
         }).catch(console.error);
     }
 
-    if(msg.content.startsWith("!gif") && msg.content != "!gif"){
+    if (msg.content.startsWith("!gif") && msg.content != "!gif") {
         var content = msg.content.replace(/!gif/i, '');
         var contentEspacos = content.trim();
         Tenor.Search.Random(contentEspacos, "1").then(Results => {
             Results.forEach(Post => {
+
                 msg.reply(Post.url);
             });
         }).catch(console.error);
@@ -42,17 +48,62 @@ client.on('message', msg => {
 
 
     // FAZ O RASTREIO DE UM CÓDIGO DOS CORREIOS E MOSTRA OS DADOS DO PACOTE
-    if(msg.content.startsWith("!rastreio") && msg.content != "!rastreio"){
-        async function exemplo(){
+    if (msg.content.startsWith("!rastreio") && msg.content != "!rastreio") {
+        async function exemplo() {
             const content = msg.content.replace(/!rastreio/i, '');
             const contentEspacos = content.trim(content);
-            
+
             const track = await rastro.track(contentEspacos);
             const rastreio = track.map((item, indice) => {
                 const local = item.tracks.map((item, indice) => {
-                    return msg.reply(`**CÓDIGO DE RASTREIO:** ${contentEspacos}
+                    if(item.status.startsWith("objeto postado")){
+                        const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail(`https://i.imgur.com/Rx52MUN.png`)
+                                .setAuthor('Correios', 'https://media.glassdoor.com/sqll/732031/empresa-brasileira-de-correios-e-telegrafos-ect-squarelogo-1576639620796.png')
+                                .setDescription(`Detalhes de envio do código **${contentEspacos}**`)
+                                .addField('status:', `${item.status}`, true)
+                                .setFooter(item.trackedAt);
 
-**${item.trackedAt}:** ${item.status} ${item.observation}`);
+                            return msg.channel.send(embed);
+                    }
+
+                    if(item.status.startsWith("objeto encaminhado")){
+                        const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail(`https://i.imgur.com/Nu9mOLv.png`)
+                                .setAuthor('Correios', 'https://media.glassdoor.com/sqll/732031/empresa-brasileira-de-correios-e-telegrafos-ect-squarelogo-1576639620796.png')
+                                .setDescription(`Detalhes de envio do código **${contentEspacos}**`)
+                                .addField('status:', `${item.status} ${item.observation}`, true)
+                                .setFooter(item.trackedAt);
+
+                            return msg.channel.send(embed);
+                    }
+
+                    if(item.status.startsWith("objeto aguardando retirada")){
+                        const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail(`https://i.imgur.com/vUQms7c.png`)
+                                .setAuthor('Correios', 'https://media.glassdoor.com/sqll/732031/empresa-brasileira-de-correios-e-telegrafos-ect-squarelogo-1576639620796.png')
+                                .setDescription(`Detalhes de envio do código **${contentEspacos}**`)
+                                .addField('status:', `${item.status} ${item.observation}`, true)
+                                .setFooter(item.trackedAt);
+
+                            return msg.channel.send(embed);
+                    }
+
+                    if(item.status.startsWith("objeto entregue")){
+                        const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail(`https://i.imgur.com/CraY6Ob.png`)
+                                .setAuthor('Correios', 'https://media.glassdoor.com/sqll/732031/empresa-brasileira-de-correios-e-telegrafos-ect-squarelogo-1576639620796.png')
+                                .setDescription(`Detalhes de envio do código **${contentEspacos}**`)
+                                .addField('status:', `${item.status}`, true)
+                                .setFooter(item.trackedAt);
+
+                            return msg.channel.send(embed);
+                    }
+                    
                 });
                 return local;
             });
@@ -62,25 +113,29 @@ client.on('message', msg => {
     }
 
     // MOSTRA OS DADOS GLOBAIS OU DO BRASIL DO COVID-19 
-    if(msg.content == "!covid -g"){
+    if (msg.content == "!covid -g") {
         var url = 'https://coronavirus-19-api.herokuapp.com/all';
         xmlhttp = new XMLHttpRequest();
 
-        xmlhttp.onreadystatechange = function() {
+        xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 var dados = xmlhttp.responseText;
                 var dadosJSON = JSON.parse(dados);
-                msg.reply(`** DADOS GLOBAIS SOBRE O COVID-19**
+                const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail('https://i.imgur.com/F61bgb1.png')
+                                .setAuthor('COVID-19 GLOBAL')
+                                .setDescription(`Dados do Covid-19 no Mundo`)
+                                .addFields(
+                                    { name: 'Casos', value: dadosJSON.cases, inline: true },
+                                    { name: 'Mortes', value: dadosJSON.deaths, inline: true },
+                                    { name: 'Recuperados', value: dadosJSON.recovered, inline: true },
+                                )
+                                .setTimestamp()
+                                .setFooter('Dados enviados');
 
-                    **casos**: ${dadosJSON.cases}
+                                msg.channel.send(embed);
 
-                    **mortes**: ${dadosJSON.deaths}
-
-                    **recuperados**: ${dadosJSON.recovered}
-
-*Não são dados exatos, todos os números podem conter diferenças de outras fontes.*
-                `);
-                
             }
         }
 
@@ -88,29 +143,31 @@ client.on('message', msg => {
         xmlhttp.send();
     }
 
-    if(msg.content == "!covid -brasil"){
+    if (msg.content == "!covid -br") {
         var url = 'https://coronavirus-19-api.herokuapp.com/countries/Brazil';
         xmlhttp = new XMLHttpRequest();
 
-        xmlhttp.onreadystatechange = function() {
+        xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 var dados = xmlhttp.responseText;
                 var dadosJSON = JSON.parse(dados);
-                msg.reply(`** DADOS SOBRE O COVID-19 DO BRASIL**
+                const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail('https://i.imgur.com/F61bgb1.png')
+                                .setAuthor('COVID-19 BRASIL')
+                                .setDescription(`Dados do Covid-19 no Brasil`)
+                                .addFields(
+                                    { name: 'Casos', value: dadosJSON.cases, inline: true },
+                                    { name: 'Mortes', value: dadosJSON.deaths, inline: true },
+                                    { name: 'Recuperados', value: dadosJSON.recovered, inline: true },
+                                    { name: 'Casos de Hoje', value: dadosJSON.todayCases, inline: true },
+                                    { name: 'Mortes de Hoje', value: dadosJSON.todayDeaths, inline: true },
+                                )
+                                .setTimestamp()
+                                .setFooter('Dados enviados');
 
-                    **casos**: ${dadosJSON.cases}
+                                msg.channel.send(embed);
 
-                    **mortes**: ${dadosJSON.deaths}
-
-                    **recuperados**: ${dadosJSON.recovered}
-
-                    **casos de hoje**: ${dadosJSON.todayCases}
-
-                    **mortes de hoje**: ${dadosJSON.todayDeaths}
-
-*Não são dados exatos, todos os números podem conter diferenças de outras fontes.*
-                `);
-                
             }
         }
 
@@ -118,20 +175,147 @@ client.on('message', msg => {
         xmlhttp.send();
     }
 
-    // EXPLICAÇÃO SOBRE OS DADOS DA API DO COVID-19
-    if(msg.content == "!covid -info"){
-        msg.reply(`**Sobre esses dados**
+    // TRAZ A COTAÇÃO ATUAL DO DÓLAR
+    if (msg.content == "!dollar") {
+        var url = "https://economia.awesomeapi.com.br/all/USD-BRL";
+        xmlhttp = new XMLHttpRequest();
 
-**Mudam rapidamente**
-*Os dados mudam rapidamente e podem não mostrar alguns dos casos que ainda não foram informados.*
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var dados = xmlhttp.responseText;
+                var dadosJSON = JSON.parse(dados);
+                var dollarMax = parseFloat(dadosJSON.USD.high);
+                var dollarMin = parseFloat(dadosJSON.USD.low);
+                const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail('https://www.iconpacks.net/icons/2/free-coin-dollar-icon-2686-thumb.png')
+                                .setAuthor('Dollar')
+                                .setDescription(`Cotação do Dollar`)
+                                .addFields(
+                                    { name: 'MAX', value: `${dollarMax.toFixed(2)}R$`, inline: true },
+                                    { name: 'MIN', value: `${dollarMin.toFixed(2)}R$`, inline: true },
+                                )
+                                .setTimestamp()
+                                .setFooter('Dados enviados');
 
-**Incluem somente as pessoas testadas**
-*Os casos incluem somente pessoas que tiveram testes com resultado positivo. As regras e a disponibilidade dos testes podem variar de acordo com o país. Talvez você não veja dados sobre algumas áreas porque não há registros da publicação ou da coleta recente dessas informações.*
+                                msg.channel.send(embed);
+            }
+        }
+        xmlhttp.open('GET', url, true);
+        xmlhttp.send();
 
-**Por que vejo dados distintos em fontes diferentes?**
-*Existem várias fontes que monitoram e agregam dados sobre o coronavírus. Essas informações são atualizadas em momentos distintos e talvez sejam coletadas de maneiras diferentes.*
-        `);
     }
+
+    // TRAZ A COTAÇÃO ATUAL DO BITCOIN
+    if (msg.content == "!bitcoin") {
+        var url = "https://economia.awesomeapi.com.br/all/BTC-BRL";
+        xmlhttp = new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                var dados = xmlhttp.responseText;
+                var dadosJSON = JSON.parse(dados);
+                var bitcoinMax = parseFloat(dadosJSON.BTC.high);
+                var bitcoinMin = parseFloat(dadosJSON.BTC.low);
+                const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail('https://images.vexels.com/media/users/3/144837/isolated/preview/40f189daa5c0279718484ca5f5569f78-bitcoin-icon-by-vexels.png')
+                                .setAuthor('Bitcoin')
+                                .setDescription(`Cotação do Bitcoin`)
+                                .addFields(
+                                    { name: 'MAX', value: `${bitcoinMax}R$`, inline: true },
+                                    { name: 'MIN', value: `${bitcoinMin}R$`, inline: true },
+                                )
+                                .setTimestamp()
+                                .setFooter('Dados enviados');
+
+                                msg.channel.send(embed);
+            }
+        }
+        xmlhttp.open('GET', url, true);
+        xmlhttp.send();
+    }
+
+    // MOSTRA A IMAGEM DE PERFIL DO USUÁRIO   
+    if (msg.content === "!avatar") {
+        const attachment = new MessageAttachment(msg.author.displayAvatarURL());
+        msg.reply(attachment);
+    }
+
+    // F NO CHAT MOSTRA R.I.P
+    if (msg.content === "F" || msg.content === "f") {
+        const attachment = new MessageAttachment("https://i.imgur.com/w3duR07.png");
+        msg.channel.send(attachment);
+    }
+
+
+    // MOSTRA O ELO NO LOL DO PLAYER INSERIDO 
+    if (msg.content.startsWith("!elo")) {
+        var content = msg.content.replace(/!elo/i, '');
+        var contentEspacos = content.trim();
+        api.get('br1', 'summoner.getBySummonerName', contentEspacos).then(data => {
+            var url = `https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${data.id}?api_key=${lolKey}`;
+            xmlhttp = new XMLHttpRequest();
+
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    var dados = xmlhttp.responseText;
+                    var dadosFormatado = dados.replace('RANKED_SOLO_5x5', 'Ranqueada Solo').replace('RANKED_FLEX_SR', 'Ranqueada Flex 5x5').replace('SILVER', 'PRATA');
+                    var dadosArray = dadosFormatado.split(', ');
+                    var dadosJSON = JSON.parse(dadosArray);
+                    dadosJSON.forEach((player) => {
+
+
+                        if (player.tier === "CHALLENGER" || player.tier === "MASTER" || player.tier === "GRANDMASTER") {
+                            const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setAuthor('League Of Legends', 'https://s3.amazonaws.com/file.imleagues/Images/Leagues/Uploaded/201803/20183612017256eacbe80750b5456c4fe8eba04dc1ffff.png')
+                                .setDescription(`Dados de **${data.name}**`)
+                                .setThumbnail(`https://ddragon.leagueoflegends.com/cdn/10.13.1/img/profileicon/${data.profileIconId}.png`)
+                                .addField('Nível', data.summonerLevel, true)
+                                .addFields(
+                                    { name: 'ELO:', value: `**${player.tier}** | ${player.queueType}` },
+                                    { name: '\u200B', value: '\u200B' },
+                                    {name: "Pdl's", value: player.leaguePoints, inline: true},
+                                    { name: 'Vitórias', value: player.wins, inline: true },
+                                    { name: 'Derrotas', value: player.losses, inline: true },
+                                )
+                                .setTimestamp()
+                                .setFooter('Dados enviados');
+
+                                msg.channel.send(embed);
+                        } else {
+                            const embed = new MessageEmbed()
+                                .setColor('#ff1a75')
+                                .setThumbnail(`https://ddragon.leagueoflegends.com/cdn/10.13.1/img/profileicon/${data.profileIconId}.png`)
+                                .setAuthor('League Of Legends', 'https://s3.amazonaws.com/file.imleagues/Images/Leagues/Uploaded/201803/20183612017256eacbe80750b5456c4fe8eba04dc1ffff.png')
+                                .setDescription(`Dados de **${data.name}**`)
+                                .addField('Nível', data.summonerLevel, true)
+                                .addFields(
+                                    { name: 'ELO:', value: `**${player.tier} ${player.rank}** | ${player.queueType}` },
+                                    { name: '\u200B', value: '\u200B' },
+                                    {name: "Pdl's", value: player.leaguePoints, inline: true},
+                                    { name: 'Vitórias', value: player.wins, inline: true },
+                                    { name: 'Derrotas', value: player.losses, inline: true },
+                                )
+                                .setTimestamp()
+                                .setFooter('Dados enviados');
+
+                                msg.channel.send(embed);
+                        }
+                    });
+                }
+            }
+            xmlhttp.open('GET', url, true);
+            xmlhttp.send();
+        });
+
+    }
+
+
+
+
+
 });
 
 
