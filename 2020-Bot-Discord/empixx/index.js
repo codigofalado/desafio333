66033a6259e1df1b91e40180b0fc273948ff7d6f
Empix <jotinhave@gmail.com>
2020-07-31 23:07:05 -0400

    heroku change

--- 2020-Bot-Discord/empixx/index.js
+++ 2020-Bot-Discord/empixx/index.js
@@ -15,20 +15,6 @@
 
 require('dotenv').config();
 
-// Heroku
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-//
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
